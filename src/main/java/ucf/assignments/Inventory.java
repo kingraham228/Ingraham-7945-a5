9932a51bf65a3dc0ecc0a45dcf5455b038bc7d62
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-07-21 11:08:22 -0400

    Add item function testing.

--- src/main/java/ucf/assignments/Inventory.java
+++ src/main/java/ucf/assignments/Inventory.java
@@ -3,11 +3,10 @@ package ucf.assignments;
  *  UCF COP3330 Summer 2021 Assignment 5 Solution
  *  Copyright 2021 Kate Ingraham
  */
-import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 
-
+//This class manages items
 public class Inventory {
     ArrayList<Item> catalog = new ArrayList<>();
 
---
+++ src/test/java/ucf/assignments/InventoryTest.java
@@ -0,0 +1,27 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InventoryTest {
+
+    @Test
+    @DisplayName("Add Item Test")
+        //Try adding an item
+    void addItem() {
+        Inventory test = new Inventory();
+
+        String name = "Test widget.";
+        String serialNumber = "DEG674WBS9";
+        String value = "$4.99";
+
+        test.addItem(name,serialNumber,value);
+
+        String actual = test.getCatalog().get(0).getName();
+        String expected = "Test widget.";
+
+        assertEquals(expected,actual);
+    }
+}
