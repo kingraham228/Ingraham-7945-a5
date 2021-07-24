c2ac46f9e6272af3e89f1cd564639723338d8028
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-07-23 23:50:22 -0400

    JSON parsing tests.

--- src/main/java/ucf/assignments/JSONFiles.java
+++ src/main/java/ucf/assignments/JSONFiles.java
@@ -4,6 +4,8 @@ package ucf.assignments;
  *  Copyright 2021 Kate Ingraham
  */
 
+import java.util.ArrayList;
+
 //This class formats data to and from JSON files
 public class JSONFiles {
 
@@ -20,5 +22,59 @@ public class JSONFiles {
     //This method formats the ending footer for a JSON file
     public String formatJSONFooter(){
         return String.format("\t]%n}");
+    }
+
+    //This method parses existing json files into inventory items
+    public ArrayList<Item> parseJSON(ArrayList<String> fileData){
+        InputValidator iv = new InputValidator();
+        ArrayList<Item> fileItems = new ArrayList<>();
+
+        for(String fileDatum: fileData){
+            String [] pieces = fileDatum.split("\",");
+            if(pieces.length==3){
+                String value = pieces[0];
+                String serial = pieces[1];
+                String name = pieces[2];
+
+                //remove json formatting for value
+                StringBuilder sbValue = new StringBuilder();
+                char [] valueTrim = value.toCharArray();
+                for (char c : valueTrim) {
+                    char decimal = '.';
+                    char comma = ',';
+                    if (Character.isDigit(c) || decimal == c || comma == c) {
+                        sbValue.append(c);
+                    }
+                }
+                value = sbValue.toString();
+
+                //remove json formatting for serial number
+                serial = serial.replaceAll("serial", "");
+                StringBuilder sbSerial = new StringBuilder();
+                char[] serialTrim = serial.toCharArray();
+                for(char c: serialTrim){
+                    if(Character.isLetterOrDigit(c)){
+                        sbSerial.append(c);
+                    }
+                }
+                serial = sbSerial.toString();
+
+                //remove json formatting for name
+                name = name.replaceAll(" \"name\" : \"", "");
+                name = name.replaceAll("\" },", "");
+                name = name.replaceAll("\" }", "");
+
+
+                //validate before adding item to the array
+                boolean valueValid = iv.checkValue(value);
+                boolean serialValid = iv.checkFormatSerial(serial);
+                boolean nameValid = iv.checkNameLength(name);
+                if(valueValid && serialValid && nameValid){
+                    Item addItem = new Item(name,serial,value);
+                    fileItems.add(addItem);
+                }
+            }
+        }
+        return fileItems;
     }
 }
---
+++ src/test/java/ucf/assignments/JSONFilesTest.java
@@ -0,0 +1,74 @@
+package ucf.assignments;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Kate Ingraham
+ */
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JSONFilesTest {
+
+    @Test
+    @DisplayName("Parse JSON: First Element check, name")
+    //try parsing a properly formatted file
+    void parseJSON() {
+        JSONFiles test = new JSONFiles();
+        ArrayList<String> testFileData = new ArrayList<>();
+        String test1 = "{\"value\" : \"$3,444.00\", \"serial\" : \"LLLWWWIII2\", \"name\" : \"item one\" },";
+        String test2 = "{\"value\" : \"$42.00\", \"serial\" : \"III222MMMA\", \"name\" : \"item two\" }";
+        testFileData.add(test1);
+        testFileData.add(test2);
+
+        ArrayList<Item> fileItems = test.parseJSON(testFileData);
+
+        //Check the first name element
+        String actual = fileItems.get(0).getName();
+        String expected = "item one";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Parse JSON: second Element check")
+        //try parsing a properly formatted file
+    void parseJSON_2serial() {
+        JSONFiles test = new JSONFiles();
+        ArrayList<String> testFileData = new ArrayList<>();
+        String test1 = "{\"value\" : \"$3,444.00\", \"serial\" : \"LLLWWWIII2\", \"name\" : \"item one\" },";
+        String test2 = "{\"value\" : \"$42.00\", \"serial\" : \"III222MMMA\", \"name\" : \"item two\" }";
+        testFileData.add(test1);
+        testFileData.add(test2);
+
+        ArrayList<Item> fileItems = test.parseJSON(testFileData);
+
+        //Check the second element serial
+        String actual = fileItems.get(1).getSerialNumber();
+        String expected = "III222MMMA";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Parse JSON: second Element check, value")
+        //try parsing a properly formatted file
+    void parseJSON_2value() {
+        JSONFiles test = new JSONFiles();
+        ArrayList<String> testFileData = new ArrayList<>();
+        String test1 = "{\"value\" : \"$3,444.00\", \"serial\" : \"LLLWWWIII2\", \"name\" : \"item one\" },";
+        String test2 = "{\"value\" : \"$42.00\", \"serial\" : \"III222MMMA\", \"name\" : \"item two\" }";
+        testFileData.add(test1);
+        testFileData.add(test2);
+
+        ArrayList<Item> fileItems = test.parseJSON(testFileData);
+
+        //Check the second element value
+        String actual = fileItems.get(1).getValue();
+        String expected = "$42.00";
+
+        assertEquals(expected, actual);
+    }
+}
